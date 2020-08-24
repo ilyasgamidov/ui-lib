7585d022c7dd984fe289f42e20e0d7df45379d8a
ilyas <gamidov@tut.by>
2020-08-24 09:46:04 +0300

    added action methods

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 import $ from './lib/lib';
 
+/* $('button').on('click', function() {
+  $('div').eq(2).toggleClass('active');
+}); */
 
-$('button').on('click', function() {
-  $(this).toggleClass('active');
+$('div').click(function() {
+  console.log($(this).index());
 });
+
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme'));
+console.log($('.some').eq(0).siblings());
+
