46188a0c6e45ded48bba1b2b48080eec87e37bc0
ilyas <gamidov@tut.by>
2020-09-14 12:25:41 +0300

    dynamic modal part 2

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -1,77 +1,85 @@
 import $ from '../core';
 
-$.prototype.modal = function() {
+$.prototype.modal = function(created) {
     for (let i = 0; i < this.length; i++) {
         const target = this[i].getAttribute('data-target');
         $(this[i]).click((e) => {
             e.preventDefault();
-            $(target).fadeIn(500); 
+            $(target).fadeIn(500);
             document.body.style.overflow = 'hidden';
         });
-    }
 
-    const closeElem = document.querySelectorAll('[data-close]');
-    closeElem.forEach(elem => {
-        $(elem).click(() => {
-            $('.modal').fadeOut(500);
-            document.body.style.overflow = '';
+        const closeElements = document.querySelectorAll(`${target} [data-close]`);
+        closeElements.forEach(elem => {
+            $(elem).click(() => {
+                $(target).fadeOut(500);
+                document.body.style.overflow = '';
+                if (created) {
+                    document.querySelector(target).remove();
+                }
+            });
         });
-    });
-
-    $('.modal').click(e => {
-        if (e.target.classList.contains('modal')) {
-            $('.modal').fadeOut(500);
-            document.body.style.overflow = '';
-        }
-    });
+    
+        $(target).click(e => {
+            if (e.target.classList.contains('modal')) {
+                $(target).fadeOut(500);
+                document.body.style.overflow = '';
+                if (created) {
+                    document.querySelector(target).remove();
+                }
+            }
+        });
+    }
 };
 
 $('[data-toggle="modal"]').modal();
 
 $.prototype.createModal = function({text, btns} = {}) {
-    for (let i = 0; i < this.length; i++) {
+    for (let i = 0; i< this.length; i++) {
         let modal = document.createElement('div');
         modal.classList.add('modal');
-        modal.setAttribute('id', this[i].getAttribute('data-targer').slice(1));
+        modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
 
-        //btns = {count: num, setting:[[text, className = [], close, cb]]}
-        const buttons = {};
-        for (let j; j < btns.count; j++) {
+        // btns = {count: num, settings: [[text, classNames=[], close, cb]]}
+        const buttons = [];
+        for (let j = 0;j < btns.count; j++){
             let btn = document.createElement('button');
-            btn.classList.add('btn', ...btns.setting[j][1]);
-            btn.textContent = btns.setting[j][0];
-
-            if (btns.setting[j][2]){
+            btn.classList.add('btn', ...btns.settings[j][1]);
+            btn.textContent = btns.settings[j][0];
+            if (btns.settings[j][2]) {
                 btn.setAttribute('data-close', 'true');
             }
-            if (btns.setting[j][3] && typeof(btns.setting[j][3] === 'funtion')){
-                btn.addEventListener('click', btns.setting[j][3]);
+            if (btns.settings[j][3] && typeof(btns.settings[j][3]) === 'function') {
+                btn.addEventListener('click', btns.settings[j][3]);
             }
 
             buttons.push(btn);
         }
 
-
         modal.innerHTML = `
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <button class="close" data-close>
-                        <span>&times;</span>
-                    </button>
-                    <div class="modal-header">
-                        <div class="modal-title">
-                            ${text.title}
-                        </div>
-                    </div>
-                    <div class="modal-body">
-                        ${text.body}
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-danger" data-close>Close</button>
-                        <button class="btn btn-succsess" >Save changes</button>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <button class="close" data-close>
+                    <span>&times;</span>
+                </button>
+                <div class="modal-header">
+                    <div class="modal-title">
+                        ${text.title}
                     </div>
                 </div>
+                <div class="modal-body">
+                    ${text.body}
+                </div>
+                <div class="modal-footer">
+                    
+                </div>
             </div>
+        </div>
         `;
+
+        modal.querySelector(".modal-footer").append(...buttons);
+        document.body.appendChild(modal);
+        $(this[i]).modal(true);
+        $(this[i].getAttribute('data-target')).fadeIn(500);
     }
 };
--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,37 @@ $('[data-count=second]').on('click', function() {
 $('button').eq(2).on('click', function() {
   $('.w-500').fadeToggle(800);
 });
+
+$('#trigger').click(() => $('#trigger').createModal({
+  text: {
+    title: 'Modal title',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.'
+  },
+  btns: {
+    count: 3,
+    settings: [
+      [
+        'Close',
+        ['btn-danger', 'mr10'],
+        true
+      ],
+      [
+        'Save changes',
+        ['btn-succsess'],
+        false,
+        () => {
+          alert('data saved');
+        }
+      ],
+      [
+        'another btn',
+        ['btn-warning', 'ml10'],
+        false,
+        () => {
+          alert('yes');
+        }
+      ]
+
+    ]
+  }
+}));
